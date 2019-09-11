db6124ec51fe66ed9ac5ac05c44e7edb93687d2d
VedantMehta74 <38360953+Debu07@users.noreply.github.com>
2019-09-11 14:22:59 +0100

    Add files via upload

---
+++ automated_calc.py
@@ -0,0 +1,5 @@
+import pyautogui
+pyautogui.click(pyautogui.locateCenterOnScreen('onekey.PNG'))
+pyautogui.click(pyautogui.locateCenterOnScreen('pluskey.PNG'))
+pyautogui.click(pyautogui.locateCenterOnScreen('onekey.PNG'))
+pyautogui.press('enter')
---
+++ mouse_controller.py
@@ -0,0 +1,26 @@
+import pyautogui
+pyautogui.FAILSAFE=True
+
+
+#moving the mouse to a specific location(in a loop)
+#for i in range(10):
+    #pyautogui.moveTo(600,600,duration=0.25)
+   # pyautogui.moveTo(1200, 600, duration=0.25)
+   # pyautogui.moveTo(1200, 1200, duration=0.25)
+   # pyautogui.moveTo(600, 1200, duration=0.25)
+# moving mouse relative to specific position
+#pyautogui.moveRel(100,0,duration=3)
+
+#getting the mouse position
+#print(pyautogui.position())
+#clicking the mouse(takes two arguments)
+#pyautogui.click(297,44)
+
+#dragging the mouse (dragging and clicking the mouse simultanoeusly)
+#moveto / moverel
+#gragto/ dragrel
+#pyautogui.moveTo(42,700)
+#pyautogui.dragTo(578,700,duration=0.3)
+
+#scrolling the mouse
+#pyautogui.scroll(200)
